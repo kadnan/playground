9dee4e13810e48be71414517d9a9898ecca8153e
Adnan Siddiqi <kadnan@gmail.com>
2017-05-28 13:54:49 +0500

    Adding file Split methods

--- file.py
+++ file.py
@@ -1,7 +1,30 @@
 import os
+import sys
 import glob
 import mimetypes
 from shutil import copyfile
+import subprocess
+
+
+def split_all(source_directory, destination_directory, extension='txt'):
+    """
+         Traverse Folder and Split files in destination
+    """
+    os.chdir(source_directory)
+    for fn in glob.glob('*.' + extension):
+        pattern = fn[:-4] + '_'
+        file_path = os.path.join(source_directory, fn)
+        dest_path = os.path.join(destination_directory, pattern)
+        split_file_by_line(file_path, 5000, 'txt', dest_path)
+
+
+def split_file_by_line(source_file, no_of_lines, extension, destination_path):
+    """
+        Split a file in number of files based on line number given
+    """
+    cmd = 'gsplit -l ' + str(
+        no_of_lines) + ' --additional-suffix=.' + extension + ' ' + source_file + ' ' + destination_path
+    result = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
 
 
 def mv_lines(source_directory, line_limit, pattern, extension, destination_directory):
@@ -11,7 +34,7 @@ def mv_lines(source_directory, line_limit, pattern, extension, destination_direc
     os.chdir(source_directory)
     files = []
     for fn in glob.glob('*.' + extension):
-        file_path = os.path.join(rootdir, fn)
+        file_path = os.path.join(source_directory, fn)
         # Is it a text file?
         m_type = mimetypes.guess_type(file_path)
 
@@ -33,9 +56,3 @@ def mv_lines(source_directory, line_limit, pattern, extension, destination_direc
     if len(files) > 0:
         [copyfile(source_directory + '/' + f, destination_directory + '/' + f) for f in files]
 
-
-rootdir = '/path/to/source'
-destdir = 'path/to/destination'
-
-# Example
-mv_lines(rootdir, 5000, '>', 'txt', destdir)
